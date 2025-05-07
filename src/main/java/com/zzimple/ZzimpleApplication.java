516e88557d99e0f187f90eb0b619fdd35fc1974c
chaeelin <chaeee0123@gmail.com>
2025-05-07 23:35:13 +0900

    :tada: Start: 프로젝트 생성

---
+++ src/main/java/com/zzimple/ZzimpleApplication.java
@@ -0,0 +1,13 @@
+package com.zzimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzimpleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZzimpleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zzimple/ZzimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzimpleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
